c78617908faf649f2c82fcfbbd15ed6d364754d5
Ramees AR <62797124+Ramees12ar@users.noreply.github.com>
2020-08-15 16:19:51 +0530

    Update settings.py

--- proj/settings.py
+++ proj/settings.py
@@ -131,8 +131,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'touchofcare.toc@gmail.com'
-EMAIL_HOST_PASSWORD = 'touchofcare'
+EMAIL_HOST_USER = 'enter your email'
+EMAIL_HOST_PASSWORD = 'enter your password'
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
